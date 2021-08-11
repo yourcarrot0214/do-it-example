80a1198e9fdeb82a4228bfe62a6a9bbb9a0243db
yourcarrot0214 <stylenbs@gmail.com>
2021-08-11 16:46:21 +0900

    4-3 스타일 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import './App.css';
 import Input from './03/Input';
+import Text from './04/Text';
+import Button from './04/Button';
 
 function App() {
   return (
     <>
-      <Input />
+      {/* <Input /> */}
+      {/* <Text>defalut</Text>
+      <Text xsmall>xsmall</Text>
+      <Text small>small</Text>
+      <Text xlarge primary>
+        xlarge
+      </Text>
+      <Text large>large</Text>
+      <Text primary>primary</Text>
+      <Text secondary>secondary</Text> */}
+      <Button small>small</Button>
+      <Button xsmall>xsmall</Button>
+      <Button large>large</Button>
+      <Button xlarge>xlarge</Button>
+      <Button primary>primary</Button>
+      <Button secondary>secondary</Button>
+      <Button xlarge primary>
+        xlarge primary
+      </Button>
+      <Button xlarge secondary>
+        xlarge secondary
+      </Button>
+      <div>
+        <Button small>small responsive</Button>
+      </div>
     </>
   );
 }
